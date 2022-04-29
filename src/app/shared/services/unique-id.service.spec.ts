cc56752a0fd8e5be93456b4521e64e0a9deb1e18
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2022-04-29 07:40:22 -0300

    Elegância nos testes complexos

--- src/app/shared/services/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id.service.spec.ts
@@ -10,6 +10,23 @@ describe(UniqueIdService.name, () => {
     //expect(id).toContain('app-');
     expect(id.startsWith('app-')).toBeTrue();
   });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate ids when called multiplestimes`, () => {
+    const service = new UniqueIdService();
+    // const firstId = service.generateUniqueIdWithPrefix('app');
+    // const secondId = service.generateUniqueIdWithPrefix('app');
+
+    // // firstId não seja secondId
+    // expect(firstId).not.toBe(secondId);
+
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+
+    // Esperamos que o tamanho do conjunto seja 50
+    expect(ids.size).toBe(50);
+  });
 });
 
 /* Estrutura de escrita de teste: #blablabla should blablabla when blablabla */
