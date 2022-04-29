c66385d2e862668d8c433d61df9dbd2a41b45da6
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2022-04-29 09:27:27 -0300

    Implementando o component de like e garantindo acessibilidade

---
+++ src/app/shared/components/like-widget/like-widget.component.ts
@@ -0,0 +1,26 @@
+import { UniqueIdService } from './../../services/unique-id.service';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+@Component({
+  selector: 'app-like-widget',
+  templateUrl: './like-widget.component.html',
+  styleUrls: ['./like-widget.component.scss'],
+})
+export class LikeWidgetComponent implements OnInit {
+  @Output() public liked = new EventEmitter<void>();
+  @Input() public likes = 0;
+  @Input() public id = null;
+  public fonts = { faThumbsUp };
+
+  constructor(private uniqueIdService: UniqueIdService) {}
+
+  public ngOnInit(): void {
+    if (!this.id) {
+      this.id = this.uniqueIdService.generateUniqueIdWithPrefix('like-wiget');
+    }
+  }
+
+  public like(): void {
+    this.liked.emit();
+  }
+}
